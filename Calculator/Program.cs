d187ee064bf602addc4d6cf722b136bc8f8f8483
Eva Leenaars <LeenaarsE@wigo4it.nl>
2025-04-15 09:54:07 +0200

    2 getallen kunnen optellen, aftrekken, vermenigvuldigen of delen

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,10 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//opdracht 1: getallen optellen
 //opdracht 2: operators zelf toevoegen
 //opdracht 3: meerdere getallen tegelijk optellen (spatie of enter)
 //nb. switch - case verdiepen
-
+//nb. functie vs method verdiepen
 
 Console.WriteLine();
 Console.WriteLine();
@@ -15,71 +14,124 @@ Console.WriteLine();
 Console.WriteLine("Welkom bij de calculator van Eva!");
 Console.WriteLine("Dit is een calculator die getallen bij elkaar optelt.");
 Console.WriteLine();
-Console.WriteLine("Een geldig getal is een getal van -1.000.000 tot en met 1.000.000.");
-Console.WriteLine("Je kunt de calculator afsluiten door een \"X\" in te voeren.");
+Console.WriteLine("Je kunt de calculator afsluiten door een \"Q\" in te voeren.");
 Console.WriteLine();
 Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 Console.WriteLine();
 
-int totaal = GetSum();
+long getal1 = GetNumber();
+long getal2 = GetNumber();
+string operatie = GetOperator();
 
-Console.WriteLine();
-Console.WriteLine("Oké, dankjewel.");
-Console.WriteLine($"Het eindtotaal is: {totaal}");
-Console.WriteLine("Tot ziens!");
+GetCalculate(getal1, getal2, operatie);
 
-
-
-static int GetSum()
+static long GetNumber()
 {
-    string? input;
-    int sum = 0;
+    Console.WriteLine("");
+    Console.WriteLine("Voer een geldig getal in of sluit af met een Q:");
+    long number;
+    bool validInput = false;
 
     do
     {
-        Console.WriteLine("Voer een geldig getal in of sluit af met een \"X\".");
-        input = Console.ReadLine();
-        bool validInput = int.TryParse(input, out int number);
+        string? input = Console.ReadLine();
 
-        switch (validInput)
+        validInput = long.TryParse(input, out number);
+
+        if (validInput == true)
         {
-            case false when input == "":
+            break;
+        }
+        else if (input?.ToUpper() == "Q")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Je hebt Q ingevoerd. De calculator wordt afgesloten.");
+            Environment.Exit(0);
+        }
+        else if (input == "")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Je hebt niets ingevoerd. Probeer het opnieuw.");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{input} is geen geldig getal. Probeer het opnieuw.");
+        }
+
+    } while (validInput == false);
+
+    return number;
+}
+
+static string GetOperator()
+{
+    Console.WriteLine("");
+    Console.WriteLine("Hoe wil je berekenen? Voer in:");
+    Console.WriteLine("A) Optellen");
+    Console.WriteLine("B) Aftrekken");
+    Console.WriteLine("C) Vermenigvuldigen");
+    Console.WriteLine("D) Delen");
+    Console.WriteLine("");
+
+    string operatie  = "";
+    bool validInput = false;
+
+    do
+    {
+        string? input = Console.ReadLine();
+
+        switch (input?.ToUpper())
+        {
+            case "A":
+                operatie = "+";
+                validInput = true;
+                break;
+            case "B":
+                operatie = "-";
+                validInput = true;
+                break;
+            case "C":
+                operatie = "*";
+                validInput = true;
+                break;
+            case "D":
+                operatie = "/";
+                validInput = true;
+                break;
+            case "Q":
+                Console.WriteLine();
+                Console.WriteLine("Je hebt Q ingevoerd. De calculator wordt afgesloten.");
+                Environment.Exit(0);
+                break;
+            case "":
                 Console.WriteLine();
                 Console.WriteLine("Je hebt niets ingevoerd. Probeer het opnieuw.");
                 break;
-
-            case false when input?.ToLower() == "x":
-                break;
-
-            case true when number > 1000000:
-                Console.WriteLine();
-                Console.WriteLine($"Het getal {number} is een te hoog getal. Probeer het opnieuw.");
-                break;
-
-            case true when number < -1000000:
-                Console.WriteLine();
-                Console.WriteLine($"Het getal {number} is een te laag getal. Probeer het opnieuw.");
-                break;
-
-            case true:
-                Console.WriteLine();
-                Console.WriteLine($"Het getal is: {number}");
-
-                sum += number;
-                Console.WriteLine($"Het totaal is op dit moment: {sum}");
-                Console.WriteLine();
-                Console.Write("Wil je er een getal bij optellen? ");
-                break;
-
             default:
                 Console.WriteLine();
-                Console.WriteLine($"{input} is geen geldig getal. Probeer het opnieuw.");
+                Console.WriteLine($"{input} is geen geldige operator. Probeer het opnieuw.");
                 break;
         }
-    }
-    while (input?.ToLower() != "x");
+    } while (validInput == false);
 
-    return sum;
+    return operatie;
 }
+
+void GetCalculate (long getal1, long getal2, string operatie)
+{
+    long resultaat = 0;
+
+    if (operatie == "+")        resultaat = getal1 + getal2;
+    else if (operatie == "-")   resultaat = getal1 - getal2;
+    else if (operatie == "*")   resultaat = getal1 * getal2;
+    else if (operatie == "/")   if (getal1 % getal2 == 0) resultaat = getal1 / getal2;
+                                else Console.WriteLine("Je kunt niet delen door 0. Probeer het opnieuw.");
+        
+
+    Console.WriteLine("");
+    Console.WriteLine($"{getal1} {operatie} {getal2} = {resultaat}");
+}
+
